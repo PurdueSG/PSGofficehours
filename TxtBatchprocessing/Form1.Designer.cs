904d94dc12ef054c88a9ec70707ce113d1b60ff8
SocratesWong <socrateswong@yahoo.com>
2015-11-03 11:21:10 -0500

    Push most version to git

--- TxtBatchprocessing/Form1.Designer.cs
+++ TxtBatchprocessing/Form1.Designer.cs
@@ -32,14 +32,14 @@
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(17, 16);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(13, 13);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Load all";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,18 +48,17 @@
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(16, 73);
-            this.listBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.listBox1.Location = new System.Drawing.Point(12, 59);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(1327, 676);
+            this.listBox1.Size = new System.Drawing.Size(1385, 550);
             this.listBox1.TabIndex = 1;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(136, 20);
+            this.button2.Location = new System.Drawing.Point(102, 16);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(56, 19);
             this.button2.TabIndex = 2;
             this.button2.Text = "Prase";
             this.button2.UseVisualStyleBackColor = true;
@@ -67,24 +66,35 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(218, 20);
+            this.button3.Location = new System.Drawing.Point(164, 16);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.Size = new System.Drawing.Size(56, 19);
             this.button3.TabIndex = 3;
             this.button3.Text = "printout";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(226, 11);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Write to file";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1360, 766);
+            this.ClientSize = new System.Drawing.Size(1409, 622);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -97,6 +107,7 @@
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- TxtBatchprocessing/Form1.cs
+++ TxtBatchprocessing/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Text.RegularExpressions;
 
 namespace TxtBatchprocessing
 {
@@ -33,9 +34,15 @@ namespace TxtBatchprocessing
         {
             IO.print();
         }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            IO.output();
+        }
     }
     public class IO{
         public static List<String> x = new List<String>();
+        public static List<String> errormakers = new List<String>();
         public static Dictionary<String, int> names = new Dictionary<String, int>();
         public static int[] hours = new int[50000];
         static int arraymax = 0;
@@ -54,39 +61,48 @@ namespace TxtBatchprocessing
 
                 int start = str.IndexOf("To: everettpberry@gmail.com") + "To: everettpberry@gmail.com".Length;
                 int end= str.IndexOf("--");
+                string work = end == -1 ? str.Substring(start) : str.Substring(start, end - start);
                 try
                 {
-                    string work = str.Substring(start, end - start);
+                    
                     work = work.Replace( "\r", "").Replace("\n","");
                     //work.Replace("\n", String.Empty);
-                    Console.Out.Write("\nStart of fun"+work);
-                    char[] delimiterChars = { '|',',' };
+                    //Console.Out.Write("\nStart of fun"+work);
+                    char[] delimiterChars = { '|',',',',' };
                     input = work.Split(delimiterChars);
-                    Console.Out.Write("Delimited Vales\n");
-                    foreach (string inpu in input) {
-                        Console.Out.Write(input + "\n");
-                    }
+                    input[0] = input[0].Trim().ToLower();
+                    //Console.Out.Write("Delimited Vales\n");
+                    
                     try
                     {
-                        if (!names.ContainsKey(input[0])) { 
+                        string resultString = Regex.Match(input[2].Trim(), @"\d+\.{0,1}\d*").Value;
+                        Console.Out.Write("\n" + resultString);
+                        int hr = (int)(Double.Parse(resultString)*60);
+                        if (!names.ContainsKey(input[0])) {
                             names.Add(input[0], arraymax++);
-                            names[input[0]] = 0;
+                            //hours[names[input[0]]] = 0;
                         }
                         //input[2] = input[2].Replace(" ", "");
-                        names[input[0]] += Int32.Parse(input[2].Trim());
 
-                        Console.Out.Write(names[input[0]] + "\t has done: " + names[input[0]]);
+
+                        
+                        hours[names[input[0]]]+=hr;
+                        Console.Out.Write(input[0] + "\t has done: " + Double.Parse(resultString)*60 + "\t Total done: " + hours[names[input[0]]] + "\n");
                     }
                     catch (Exception e) {
-                        Console.Out.Write("\nInner Error");
-                        foreach (string stri in input) {
-                            Console.Out.Write(stri+"\t");
-                        }
-                        Console.Out.Write("\n");
+                        Console.Out.Write("\n============================Inner Error=========================\n");
+                        errormakers.Add(str);
+                        Console.Out.Write(work+"\n");
+                        Console.Out.Write(e.ToString()+"\n");
+                        Console.Out.Write("\n***************************End Error****************************\n");
                     }
                 }
                 catch (Exception e) {
-                    Console.Out.Write("\n"+"Outer error"+"\n"+ str);
+                    Console.Out.Write("\n"+"#################################Outer error#########################");
+                    Console.Out.Write(start+"End:"+end+"\n"+ str);
+                    errormakers.Add(str);
+                    Console.Out.Write(e.ToString()+"\n");
+                    Console.Out.Write("\n***************************End Error****************************\n");
                 }
                 
             }
@@ -95,11 +111,31 @@ namespace TxtBatchprocessing
             foreach (KeyValuePair<string, int> entry in names)
             {
                 Console.Out.Write("Name: " + entry.Key + " Has Done: " + hours[entry.Value]); Console.Out.Write("\n");
+                
                 // do something with entry.Value or entry.Key
             }
             
         }
-    
+        public static void output() {
+            StreamWriter writetext = new StreamWriter("output.csv");
+            foreach (KeyValuePair<string, int> entry in names)
+            {   
+                //Console.Out.Write("Name: "+ entry.Key + " Has Done: " + hours[entry.Value]);
+                writetext.WriteLine(entry.Key + ",{0:f3}",hours[entry.Value]/60.000);
+                // do something with entry.Value or entry.Key
+            }
+            writetext.Close();
+            writetext = new StreamWriter("idiots.csv");
+            foreach (String entry in errormakers)
+            {
+                writetext.WriteLine("\n============================Start Error=========================\n");
+                //Console.Out.Write("Name: "+ entry.Key + " Has Done: " + hours[entry.Value]);
+                writetext.WriteLine( entry );
+                // do something with entry.Value or entry.Key
+                writetext.WriteLine("\n****************************End Error***************************\n");
+            }
+            writetext.Close();
+        }
     
     
     
