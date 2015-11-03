9a329e033087e36c00391ff4496565cee98a7057
SocratesWong <socrateswong@yahoo.com>
2015-11-03 11:59:30 -0500

    CC licenses

--- TxtBatchprocessing/Program.cs
+++ TxtBatchprocessing/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*  2015(c) Socrates Wong
+ *  Permissiable use granted under Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International, written permission required for other usage.   
+ * 
+ * 
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
